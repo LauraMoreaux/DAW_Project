e5fe04c1cfc298738ca7d44d2db1588a42f37f89
lauramoreaux <lauramoreauxcueto@gmail.com>
2023-04-22 17:39:11 +0200

    feat(components): add header and footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
+import { Divider, Box, Container, Grid } from "@mui/material";
 
 const Footer = () => {
-  return <div>Esto es el footer</div>;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "auto",
+        paddingTop: "1rem",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Divider />
+        <Grid container direction="column" alignItems="center" mt={3}>
+          <Grid item xs={12}>
+            <Typography color="black" variant="h5">
+              Mentor Match Web App
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="textSecondary" variant="subtitle1">
+              {`${new Date().getFullYear()} | React | Material UI | React Router`}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 };
 
 export default Footer;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
-  return <div>Esto es el header</div>;
+  return (
+    <AppBar>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Link to={"/"}>
+          <Typography variant="h6" component="div">
+            Mentor Match
+          </Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
