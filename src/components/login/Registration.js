850667f508d36c4b61d428e2c6afb2c2b87219bc
Laura <lauramoreauxcueto@gmail.com>
2023-05-20 13:23:22 +0200

    feat(registration): add loading render and url forced when logged in

--- src/components/login/Registration.js
+++ src/components/login/Registration.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import {
@@ -11,16 +11,22 @@ import {
   RadioGroup,
   Radio,
   FormControl,
+  CircularProgress,
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import { addDoc, collection } from "@firebase/firestore";
 import firestore from "../../firebase";
+import { AuthContext } from "../../context/auth-context";
 
 const Registration = () => {
   const navigate = useNavigate();
+  const { state, login } = useContext(AuthContext);
+  const { loggedUser, isLoginPending } = state;
   const ref = collection(firestore, "users");
+
+  //handle form submit
   const handleSubmit = async (event) => {
     event.preventDefault();
     //form data
@@ -33,117 +39,133 @@ const Registration = () => {
     // firebase
     const newUser = await addDoc(ref, formattedData);
     if (newUser.id) {
-      localStorage.setItem("user", JSON.stringify(data.get("email")));
-      localStorage.setItem("userID", JSON.stringify(newUser.id));
+      login();
       navigate("/my-account");
     }
   };
-  return (
-    <>
-      <Container component="main" maxWidth="xs">
-        <Stack className={"main-div"}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              ¡Regístrate!
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <FormControl>
-                    <RadioGroup
-                      aria-labelledby="demo-radio-buttons-group-label"
-                      defaultValue="mentorizado"
-                      name="mentor"
-                      sx={{ flexDirection: "row" }}
-                    >
-                      <FormControlLabel
-                        value="mentor"
-                        control={<Radio />}
-                        label="Mentor"
-                      />
-                      <FormControlLabel
-                        value="mentorizado"
-                        control={<Radio />}
-                        label="Mentorizado"
-                      />
-                    </RadioGroup>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="family-name"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link to={"/login"}>¿Ya tienes cuenta?</Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
+  if (isLoginPending) {
+    return (
+      <Container component="main">
+        <Stack className={"main-div"} display={"flex"} alignItems={"center"}>
+          <CircularProgress />
         </Stack>
       </Container>
-    </>
+    );
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Stack className={"main-div"}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {!!loggedUser ? (
+            <>
+              Ya has hecho login con una cuenta registrada, disfruta YA de las
+              ventajas clicando<Link to="/"> aquí </Link>
+            </>
+          ) : (
+            <>
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                ¡Regístrate!
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 3 }}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <FormControl>
+                      <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="mentorizado"
+                        name="mentor"
+                        sx={{ flexDirection: "row" }}
+                      >
+                        <FormControlLabel
+                          value="mentor"
+                          control={<Radio />}
+                          label="Mentor"
+                        />
+                        <FormControlLabel
+                          value="mentorizado"
+                          control={<Radio />}
+                          label="Mentorizado"
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      autoComplete="given-name"
+                      name="firstName"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="family-name"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="new-password"
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign Up
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item>
+                    <Link to={"/login"}>¿Ya tienes cuenta?</Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </>
+          )}
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
