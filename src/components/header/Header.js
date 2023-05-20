fbd36a379e8629de2301c5b5c5183abdb1530759
Laura <lauramoreauxcueto@gmail.com>
2023-05-20 13:22:26 +0200

    feat(styles): amend styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,7 +7,7 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { AuthContext } from "../../context/auth-context";
-import { Tooltip } from "@mui/material";
+import { Stack, Tooltip } from "@mui/material";
 
 const Header = () => {
   const { state, logout, isLoginPending } = useContext(AuthContext);
@@ -15,45 +15,48 @@ const Header = () => {
 
   return (
     <AppBar>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
+      <Toolbar>
         {isLoginPending ? (
           <></>
         ) : (
           <>
-            <Link to={"/my-account"}>
-              <Tooltip title={"Ir a tu cuenta"}>
-                <IconButton
-                  size="large"
-                  edge="start"
-                  color="inherit"
-                  aria-label="menu"
-                  sx={{ mr: 2 }}
-                >
-                  <MenuIcon />
-                </IconButton>
-              </Tooltip>
-            </Link>
-            <Link to={"/"}>
-              <Typography variant="h6" component="div">
-                Mentor Match
-              </Typography>
-            </Link>
-            {!!loggedUser && (
-              <Link to={"/"}>
-                <Tooltip title={"Cerrar sesión"}>
+            <Stack flex={1} flexDirection={"row"} alignItems={"center"}>
+              <Link to={"/my-account"}>
+                <Tooltip title={"Ir a tu cuenta"}>
                   <IconButton
                     size="large"
                     edge="start"
                     color="inherit"
                     aria-label="menu"
                     sx={{ mr: 2 }}
-                    onClick={() => logout()}
                   >
-                    <LogoutIcon />
+                    <MenuIcon />
                   </IconButton>
                 </Tooltip>
               </Link>
-            )}
+              <Link to={"/"}>
+                <Typography variant="h6" component="div">
+                  Mentor Match
+                </Typography>
+              </Link>
+            </Stack>
+            <Stack>
+              {!!loggedUser && (
+                <Link to={"/"}>
+                  <Tooltip title={"Cerrar sesión"}>
+                    <IconButton
+                      size="large"
+                      edge="start"
+                      color="inherit"
+                      aria-label="menu"
+                      onClick={() => logout()}
+                    >
+                      <LogoutIcon />
+                    </IconButton>
+                  </Tooltip>
+                </Link>
+              )}
+            </Stack>
           </>
         )}
       </Toolbar>
